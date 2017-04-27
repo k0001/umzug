cfba504037adc45c400bb8a153d4be77df068a5d
Renzo Carbonara <gnuk0001@gmail.com>
2017-04-27 13:31:21 +0200

    nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,1 @@
-{ pkgs ? import <nixpkgs> {}
-, compiler ? "ghc801"
-}:
-
-let
-
-  f = import ./default.nix;
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage f {};
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+(import ./default.nix).env
